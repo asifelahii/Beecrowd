9035a4ff477af23412e971d05afad35f609ff025
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-06-26 21:55:12 +0600

    Problem 1042

---
+++ 1042.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,max;
+    scanf("%d %d %d",&a,&b,&c);
+    max = a;
+
+    if(b>a && b>c){
+        max = b;
+    }
+    if(c>a && c>b){
+        max = c;
+    }
+
+    int max2 = a;
+    if(max>a && ){
+        
+    }
+
+
+
+    printf("max value = %d\n",max);
+
+    printf("\n%d\n%d\n%d\n",a,b,c);
+
+
+    return 0;
+}
