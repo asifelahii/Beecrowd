7ffc9bc175c567f3c83933573fd38e6d325a72e5
ASIF ELAHI <149480370+asifelahii@users.noreply.github.com>
2024-07-15 23:21:13 +0600

    Add files via upload

---
+++ Domino_piling.cpp
@@ -0,0 +1,17 @@
+// Link: https://codeforces.com/problemset/problem/50/A
+// A. Domino piling
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int m, n, board, dominoes;
+    cin >> m >> n;
+    board = m*n;
+    dominoes = board/2;
+
+    cout << dominoes << endl;
+    
+    return 0;
+}
